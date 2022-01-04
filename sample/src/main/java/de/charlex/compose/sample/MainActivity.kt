f8c285ff067648c5259eb08fb3c1899bae7596a8
Julian Kalinowski <Julian.Kalinowski@dataport.de>
2022-01-04 14:56:49 +0100

    docs: add complex examples

--- sample/src/main/java/de/charlex/compose/sample/MainActivity.kt
+++ sample/src/main/java/de/charlex/compose/sample/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,43 +43,50 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background,
                     contentColor = colors.onPrimary
                 ) {
-                    RevealSamples(
-                        items = listOf(
-                            Item(
-                                label = "Both directions",
-                                color = colors.one,
-                                directions = setOf(
-                                    RevealDirection.StartToEnd,
-                                    RevealDirection.EndToStart,
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .verticalScroll(rememberScrollState())
+                    ) {
+
+                        RevealSamples(
+                            items = listOf(
+                                Item(
+                                    label = "Both directions",
+                                    color = colors.one,
+                                    directions = setOf(
+                                        RevealDirection.StartToEnd,
+                                        RevealDirection.EndToStart,
+                                    ),
                                 ),
-                            ),
-                            Item(
-                                label = "Both directions, closeOnClick = false",
-                                color = colors.two,
-                                directions = setOf(
-                                    RevealDirection.StartToEnd,
-                                    RevealDirection.EndToStart,
+                                Item(
+                                    label = "Both directions, closeOnClick = false",
+                                    color = colors.two,
+                                    directions = setOf(
+                                        RevealDirection.StartToEnd,
+                                        RevealDirection.EndToStart,
+                                    ),
+                                    closeOnClick = false,
                                 ),
-                                closeOnClick = false,
-                            ),
-                            Item(
-                                label = "StartToEnd",
-                                color = colors.three,
-                                directions = setOf(
-                                    RevealDirection.StartToEnd,
-    //                                RevealDirection.EndToStart,
+                                Item(
+                                    label = "StartToEnd",
+                                    color = colors.three,
+                                    directions = setOf(
+                                        RevealDirection.StartToEnd,
+                                    ),
                                 ),
-                            ),
-                            Item(
-                                label = "EndToStart",
-                                color = colors.four,
-                                directions = setOf(
-    //                                RevealDirection.StartToEnd,
-                                    RevealDirection.EndToStart,
-                                ),
+                                Item(
+                                    label = "EndToStart",
+                                    color = colors.four,
+                                    directions = setOf(
+                                        RevealDirection.EndToStart,
+                                    ),
+                                )
                             )
                         )
-                    )
+
+                        ComplexRevealSamples()
+                    }
                 }
             }
         }
@@ -83,27 +96,16 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun RevealSamples(items: List<Item>) {
-    Column(
-        modifier = Modifier.padding( 16.dp)
-    ) {
+    Column() {
         items.forEach { item ->
             RevealSwipe(
                 modifier = Modifier.padding(vertical = 5.dp),
                 directions = item.directions,
                 hiddenContentStart = {
-                    Icon(
-                        modifier = Modifier.padding(horizontal = 25.dp),
-                        imageVector = Icons.Outlined.Star,
-                        contentDescription = null,
-                        tint = Color.White
-                    )
+                    Star()
                 },
                 hiddenContentEnd = {
-                    Icon(
-                        modifier = Modifier.padding(horizontal = 25.dp),
-                        imageVector = Icons.Outlined.Delete,
-                        contentDescription = null
-                    )
+                    Trash()
                 },
                 closeOnContentClick = item.closeOnClick
             ) {
@@ -117,7 +119,6 @@ fun RevealSamples(items: List<Item>) {
                             .fillMaxWidth(),
                         contentAlignment = Alignment.CenterStart
                     ) {
-
                         Text(
                             modifier = Modifier.padding(start = 20.dp),
                             text = item.label
@@ -127,6 +128,113 @@ fun RevealSamples(items: List<Item>) {
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun ComplexRevealSamples() {
+    Column() {
+        TextFieldRevealSwipe()
+        ButtonRevealSwipe()
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun ButtonRevealSwipe() {
+    RevealSwipe(
+        modifier = Modifier.padding(vertical = 5.dp),
+        hiddenContentStart = {
+            Star()
+        },
+        hiddenContentEnd = {
+            Trash()
+        },
+    ) {
+        Card(
+            shape = it,
+            backgroundColor = colors.two
+        ) {
+            Row(
+                modifier = Modifier
+                    .height(80.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+
+                Spacer(modifier = Modifier.width(16.dp))
+                val state = remember { mutableStateOf(false) }
+                Button(onClick = { state.value = !state.value }) {
+                    Text("Click me!")
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(if (state.value) "Clicked!" else "")
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun TextFieldRevealSwipe() {
+    RevealSwipe(
+        modifier = Modifier.padding(vertical = 5.dp),
+        hiddenContentStart = {
+            Star()
+        },
+        hiddenContentEnd = {
+            Trash()
+        },
+//        contentClickHandledExtern = true
+    ) {
+        Card(
+            shape = it,
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(80.dp)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.CenterStart
+            ) {
+
+                val text = remember { mutableStateOf("") }
+                BasicTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp),
+                    value = text.value,
+                    onValueChange = { text.value = it },
+                    decorationBox = {
+                        it()
+                        if (text.value.isBlank()) {
+                            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
+                                Text("Enter Name")
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun Trash() {
+    Icon(
+        modifier = Modifier.padding(horizontal = 25.dp),
+        imageVector = Icons.Outlined.Delete,
+        contentDescription = null
+    )
+}
+
+@Composable
+private fun Star() {
+    Icon(
+        modifier = Modifier.padding(horizontal = 25.dp),
+        imageVector = Icons.Outlined.Star,
+        contentDescription = null,
+        tint = Color.White
+    )
 }
 
 @Preview(showBackground = true)
